621d0cf4b5c938c2c406b7b5a6c314a87f812580
budmc29 <chirica.mugurel@gmail.com>
2016-01-02 23:41:58 +0000

    initialize repository

---
+++ readme.md
@@ -0,0 +1,6 @@
+# Install base programs on a new ubuntu machine
+
+Usage:
+```
+sudo bash ./ubuntu-fresh-install.bash
+# Ubuntu fresh install
---
+++ ubuntu-fresh-install.bash
@@ -0,0 +1,35 @@
+#!/bin/bash
+apt-get update  # To get the latest package lists
+apt-get upgrade  # To get the latest package lists
+
+# install console fonts
+curl https://gist.githubusercontent.com/lucasdavila/3875946/raw/1c100cae16a06bef154af0f290d665405b554b3b/install_source_code_pro.sh | sh 
+echo "console font installed"
+
+# install zsh and oh-my-zsh
+apt-get install zsh 
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+echo "zsh and oh-my-zsh installed"
+
+# copy dotfiles
+cp ~/ubuntu-dotfiles/.zshrc ~/.zshrc
+dotupdate # copy all dotfiles from directory to ~/
+
+# install gvim
+apt-get install vim-gtk
+echo "gvim installed"
+vundle # install plugin manager for vim
+
+mkdir ~/.vim/undo
+mkdir ~/.vim/swap
+
+# tmux 2.0
+apt-get install -y python-software-properties software-properties-common
+add-apt-repository -y ppa:pi-rho/dev
+apt-get update
+sudo apt-get install -y tmux=2.0-1~ppa1~t
+echo 'tmux 2.0 installed'
+
+# install tmux plugins
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+# tmux source ~/.tmux.conf
