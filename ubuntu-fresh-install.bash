3c8e6d06821fc75878963dcc748d50956220ac60
budmc29 <chirica.mugurel@gmail.com>
2016-11-07 21:05:25 +0000

    change install order

--- ubuntu-fresh-install.bash
+++ ubuntu-fresh-install.bash
@@ -8,8 +8,18 @@ apt-get upgrade  # To get the latest package lists
 #TODO: remember to display messages of confirmation
 #TODO: make zsh the defaul shell
 
+
+# config git
+git config --global user.email "chirica.mugurel@gmail.com"
+git config --global user.name "budmc29"
+
+# install mercurial
+apt-get install mercurial -y
+
+hg clone http://bitbucket.org/sjl/hg-prompt/ /usr/local/hg-plugins/prompt
+
 # install console fonts
-curl https://gist.githubusercontent.com/lucasdavila/3875946/raw/1c100cae16a06bef154af0f290d665405b554b3b/install_source_code_pro.sh | sh 
+curl https://gist.githubusercontent.com/lucasdavila/3875946/raw/1c100cae16a06bef154af0f290d665405b554b3b/install_source_code_pro.sh | sh
 echo "console font installed"
 
 # install gvim
@@ -22,6 +32,48 @@ mkdir ~/.vim/swap
 
 # install silver search for vim plugin
 apt-get install silversearcher-ag
+
+# install tmux plugins
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# install antivirus
+`apt-get install clamav -y
+freshclam`
+echo 'clam antivirus installed'
+
+# install RVM
+# key to verify the installed version
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+
+\curl -sSL https://get.rvm.io | bash -s stable --ruby
+
+apt-get install xclip -y
+apt-get install vlc -y
+apt-get install chromium-browser -y
+apt-get install imagemagick -y
+apt-get install filezilla -y
+apt-get install mysql-client -y
+apt-get install mysql-workbench
+apt-get install nodejs -y
+apt-get install apache2 -y
+
+# TODO
+# install firefox38.bash
+# install i3wm.bash
+
+# copy dotfiles
+git clone https://github.com/budmc29/ubuntu-dotfiles ~/ubuntu-dotfiles
+cp ~/ubuntu-dotfiles/.zshrc ~/.zshrc
+
+source ~/.zshrc
+
+`dotupdate` # copy all dotfiles from directory to ~/
+
+# Vim plugin manager
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# install vim plugins
+vim +PluginInstall +qall
 
 # tmux 2.2
 VERSION=2.2
@@ -52,50 +104,7 @@ sudo rm -rf /usr/local/src/tmux-*
 sudo mv tmux-${VERSION} /usr/local/src
 fi
 
-# install tmux plugins
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
-# install irc chat for tmux
-# apt-get install irssi -y 
-# echo 'issi installed'
-
-# install antivirus
-apt-get install clamav -y
-freshclam
-echo 'clam antivirus installed'
-
-# install RVM
-# key to verify the installed version
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-
-\curl -sSL https://get.rvm.io | bash -s stable --ruby
-
-apt-get install xclip -y
-apt-get install vlc -y
-apt-get install mercurial -y
-apt-get install chromium-browser -y
-apt-get install imagemagick -y
-apt-get install filezilla -y
-apt-get install skype -y
-apt-get install mysql-client -y
-apt-get install mysql-workbench
-apt-get install nodejs -y
-apt-get install apache2 -y
-
-# TODO
-# install firefox38.bash
-# install i3wm.bash
-
-# copy dotfiles
-git clone https://github.com/budmc29/ubuntu-dotfiles ~/ubuntu-dotfiles
-cp ~/ubuntu-dotfiles/.zshrc ~/.zshrc
-dotupdate # copy all dotfiles from directory to ~/
-
-# Vim plugin manager
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
 # install zsh and oh-my-zsh
 apt-get install zsh 
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 echo "zsh and oh-my-zsh installed"
-
