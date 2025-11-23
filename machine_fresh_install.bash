95a7639cc75f3d1e58a4c0a0007af76cc2fd8155
Theo Chirica <theo.m.chirica@gmail.com>
2025-11-23 16:33:02 +0000

    Run oh-my-zsh installer without auto shell

--- machine_fresh_install.bash
+++ machine_fresh_install.bash
@@ -95,7 +95,7 @@ install_fonts() {
 zsh_setup() {
   rm -rf /home/$user/.oh-my-zsh
 
-  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  RUNZSH=no sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
   sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
